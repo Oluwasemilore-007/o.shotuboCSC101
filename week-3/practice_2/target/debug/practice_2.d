7ee8f7d492fc27fa34716d7182d119ee7d7a82fc
Semilore101 <semshotubo@gmail.com>
2024-10-31 20:08:34 +0100

    (Practices 1-10)

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\semzi\Documents\o.shotuboCSC101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\semzi\Documents\o.shotuboCSC101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\semzi\Documents\o.shotuboCSC101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\semzi\Documents\o.shotuboCSC101\week-3\practice_9\src\main.rs
